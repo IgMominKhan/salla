eb9e95c95781b1d2e3af1381e5e037dbb6ef0627
IgMominKhan <mominkhan8991@gmail.com>
2023-09-11 13:00:26 +0300

    product_id props type changed

--- src/components/AddToCart.tsx
+++ src/components/AddToCart.tsx
@@ -1,5 +1,5 @@
 import { BsCart2 } from "react-icons/bs";
-const AddToCart = ({ product_id }) => {
+const AddToCart = ({ product_id }: { product_id: number }) => {
   return (
     <button className="px-xl @[16rem]:px-12 py-xl text-clr-dark-gray-600 flex gap-lg items-center border border-clr-dark-gray-100 rounded-[4px] whitespace-nowrap">
       أضف للسلة
